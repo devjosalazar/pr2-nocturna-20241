e7e58ce67334063c00f180e44c1bca7379cc47eb
josalazar <53492638+devjosalazar@users.noreply.github.com>
2024-03-06 20:37:40 -0500

    Add singleton

---
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/programacion2/creacional/singleton/Main.java
@@ -0,0 +1,19 @@
+package co.edu.uniquindio.programacion2.creacional.singleton;
+
+import co.edu.uniquindio.programacion2.creacional.singleton.model.Conexion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Instanciación por constructor prohíbido por ser "private"
+        //Conexion c = new Conexion();
+        Conexion c = Conexion.getInstancia();
+        c.conectar();
+        c.desconectar();
+
+        Conexion c2 = Conexion.getInstancia();
+
+        boolean rpta = c instanceof Conexion;
+        System.out.println(rpta);
+    }
+}
